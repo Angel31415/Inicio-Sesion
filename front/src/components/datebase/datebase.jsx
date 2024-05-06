badb418688595aa1d7ad852e4a1a7df1671fc9b4
Angel31415 <131670585+Angel31415@users.noreply.github.com>
2024-05-06 08:20:31 -0500

    primer commit
    transferencia de archivos

---
+++ front/src/components/datebase/datebase.jsx
@@ -0,0 +1,10 @@
+export let usuarios = [
+    {
+        user: "admin",
+        contrasena: "1234"
+    },
+    {
+        user: 'angel',
+        contrasena: "1234" 
+    }
+]
---
+++ front/src/components/layouts/auth/Login.jsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+import { usuarios } from "../../database/dataBase.jsx";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./Login.css";
+let urlUsuarios = "http://localhost:3000/user";
+const Login = () => {
+  const [getUsuario, setUsuario] = useState("");
+  const [getContrasena, setContrasena] = useState("");
+  const [usuarios, setUsuarios] = useState([]);
+  const redireccion = useNavigate();
+  async function getUsuarios() {
+    let resultado = await axios.get(urlUsuarios);
+    console.log(resultado.data);
+    setUsuarios(resultado.data);
+  }
+  function validarInicioSesion() {
+    if (buscarUsuario()) {
+      console.log("Inicio de sesión correcta");
+      redireccion("/home");
+    } else {
+      console.log("Error de credenciales");
+    }
+  }
+  useEffect(() => {
+    getUsuarios();
+  }, []);
+  function buscarUsuario() {
+    return usuarios.some(
+      (usuario) =>
+        usuario.user === getUsuario && usuario.contrasena === getContrasena
+    );
+  }
+  return (
+    <form>
+      <div className="container fadeInAnimation">
+        <h2>Iniciar Sesión</h2>
+        <div className="input-group">
+          <label htmlFor="username">Usuario:</label>
+          <input
+            type="text"
+            id="username"
+            onChange={(e) => {
+              setUsuario(e.target.value);
+            }}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Contraseña:</label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => {
+              setContrasena(e.target.value);
+            }}
+          />
+        </div>
+        <section className="botones">
+          <button onClick={validarInicioSesion} type="button" className="btn">
+            Iniciar Sesión
+          </button>
+          <Link to="/registro" type="button" className="btn">
+            Crear Cuenta
+          </Link>
+        </section>
+      </div>
+    </form>
+  );
+};
+export default Login;
